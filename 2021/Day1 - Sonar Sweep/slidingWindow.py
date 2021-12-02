fc56e877e8dd622e15ada8e7b5f686a12a7581e5
lemondrop81 <hacker814@hotmail.com>
2021-12-02 08:55:10 -0600

    Work on logic for the loop and if statement

--- 2021/Day1 - Sonar Sweep/slidingWindow.py
+++ 2021/Day1 - Sonar Sweep/slidingWindow.py
@@ -4,9 +4,18 @@ def start():
     text_file = open("2021\Day1 - Sonar Sweep\Day1.txt","r")
     depths = list(map(int, text_file.readlines()))
 
-    i = range(0, len(depths))
+    x = range(0, len(depths))
     
     # Adding to the running total of larger numbers
     total = 0
+
+    # The for loop to iterate through every item
+    for i in x:
+
+        # Making sure the loop doesn't go past the end
+        if i < (max(x) - 2):
+            
+
+
     
 start()

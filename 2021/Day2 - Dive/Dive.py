170d40eae1ba0f5987f0b29d5bd665d3c42bd348
lemondrop81 <hacker814@hotmail.com>
2021-12-02 09:07:56 -0600

    Set intial positions

--- 2021/Day2 - Dive/Dive.py
+++ 2021/Day2 - Dive/Dive.py
@@ -3,5 +3,10 @@ def start():
     # Getting the text input
     text_file = open("2021\Day2 - Dive\Day2.txt","r")
 
+    # Adding to the running total of horizontal position
+    horizontal = 0
 
+    # Adding to the running total of depth position
+    depth = 0
+    
 start()

c38f5b3ba01e348c89bb8b1b824d7098ea0171b6
lemondrop81 <hacker814@hotmail.com>
2021-12-02 08:51:21 -0600

    Get the text input

---
+++ 2021/Day1 - Sonar Sweep/slidingWindow.py
@@ -0,0 +1,8 @@
+def start():
+    
+    # Getting the text input
+    text_file = open("2021\Day1 - Sonar Sweep\Day1.txt","r")
+    depths = list(map(int, text_file.readlines()))
+
+    
+start()
--- 2021/Day1 - Sonar Sweep/sonar.py
+++ 2021/Day1 - Sonar Sweep/sonar.py
@@ -1,6 +1,4 @@
 def start():
-    values = [1, 2, 1, 4, 5]
-
     # Getting the text input
     text_file = open("2021\Day1 - Sonar Sweep\Day1.txt","r")
     depths = list(map(int, text_file.readlines()))

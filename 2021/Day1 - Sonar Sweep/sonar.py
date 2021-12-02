8bd99f62f78e5b3b38ae69e6591c870837c74ae0
lemondrop81 <hacker814@hotmail.com>
2021-12-02 08:13:42 -0600

    Setting up the for loop and logic

--- 2021/Day1 - Sonar Sweep/sonar.py
+++ 2021/Day1 - Sonar Sweep/sonar.py
@@ -1,4 +1,10 @@
 def start():
-    print("test")
+    values = [1, 2, 1, 4, 5]
+
+    for x in values:
+        if values[x] < values[x-1]:
+            print("smaller")
+        else:
+            print("larger")
 
 start()

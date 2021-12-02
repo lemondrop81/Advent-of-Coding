160fd9bf8e01c2f6ec2d71d904305329ff4f0cc5
lemondrop81 <hacker814@hotmail.com>
2021-12-02 09:11:24 -0600

    Read the lines, and gets the range

--- 2021/Day2 - Dive/Dive.py
+++ 2021/Day2 - Dive/Dive.py
@@ -2,11 +2,15 @@ def start():
 
     # Getting the text input
     text_file = open("2021\Day2 - Dive\Day2.txt","r")
+    lines = text_file.readlines()
+    print(lines)
+
+    x = range(0, len(lines))
 
     # Adding to the running total of horizontal position
     horizontal = 0
 
     # Adding to the running total of depth position
     depth = 0
-    
+
 start()

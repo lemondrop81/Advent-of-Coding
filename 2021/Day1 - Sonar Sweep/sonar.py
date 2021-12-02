aa5f2d51c1c22259a0dae40d775371a716b156b3
lemondrop81 <hacker814@hotmail.com>
2021-12-02 08:48:57 -0600

    Completed part 1

--- 2021/Day1 - Sonar Sweep/sonar.py
+++ 2021/Day1 - Sonar Sweep/sonar.py
@@ -3,9 +3,9 @@ def start():
 
     # Getting the text input
     text_file = open("2021\Day1 - Sonar Sweep\Day1.txt","r")
-    
+    depths = list(map(int, text_file.readlines()))
 
-    i = range(0, len(values))
+    i = range(0, len(depths))
     
     # Adding to the running total of larger numbers
     total = 0
@@ -17,10 +17,10 @@ def start():
         if x < max(i):
 
             #Checks to see if the value is larger then the previous one
-            if values[x] < values[x+1]:
-                print("larger")
+            if depths[x] < depths[x+1]:
 
                 total += 1
-
+    
+    #Print the total number
     print(total)
 start()

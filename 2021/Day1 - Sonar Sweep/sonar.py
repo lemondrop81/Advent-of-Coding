93286fd2b62eddf7b866e2340fe863cf8a7209b1
lemondrop81 <hacker814@hotmail.com>
2021-12-02 08:31:47 -0600

    If statements

--- 2021/Day1 - Sonar Sweep/sonar.py
+++ 2021/Day1 - Sonar Sweep/sonar.py
@@ -1,10 +1,16 @@
 def start():
     values = [1, 2, 1, 4, 5]
 
-    for x in values:
-        if values[x] < values[x-1]:
-            print("smaller")
-        else:
-            print("larger")
+    i = range(0, len(values))
+    
+    total = 0
+
+    for x in i:
+        if x < max(i):
+            if values[x] < values[x+1]:
+                print("smaller")
+
+            else:
+                print("larger")
 
 start()

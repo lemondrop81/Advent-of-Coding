a9d51f2d96a3af6520e2dc582bf8e3c5105fd6d8
lemondrop81 <hacker814@hotmail.com>
2021-12-02 08:51:59 -0600

    Add range and running total

--- 2021/Day1 - Sonar Sweep/slidingWindow.py
+++ 2021/Day1 - Sonar Sweep/slidingWindow.py
@@ -1,8 +1,12 @@
 def start():
-    
+
     # Getting the text input
     text_file = open("2021\Day1 - Sonar Sweep\Day1.txt","r")
     depths = list(map(int, text_file.readlines()))
 
+    i = range(0, len(depths))
+    
+    # Adding to the running total of larger numbers
+    total = 0
     
 start()

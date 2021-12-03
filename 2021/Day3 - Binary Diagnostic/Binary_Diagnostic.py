b0b363180ac5bd490d775a816b91dcb0780d83f8
lemondrop81 <hacker814@hotmail.com>
2021-12-03 10:18:05 -0600

    Setting up the file input

---
+++ 2021/Day3 - Binary Diagnostic/Binary_Diagnostic.py
@@ -0,0 +1,12 @@
+def start():
+
+    # Getting the text input
+    text_file = open("2021\Day3 - Binary Diagnostic\Day3.txt","r")
+    lines = text_file.readlines()
+
+    gamma = ""
+    epsilon = ""
+
+
+
+start()

bea6b105f605b1bdb378598be373f95b05763580
lemondrop81 <hacker814@hotmail.com>
2021-12-08 08:08:58 -0600

    Loop through the number of days

--- 2021/Day6 - Lanternfish/lanternfish.py
+++ 2021/Day6 - Lanternfish/lanternfish.py
@@ -3,6 +3,11 @@ def start():
     # Getting the text input
     text_file = open("2021\Day6 - lanternfish\Day6.txt","r")
     lines = text_file.readlines()
+
+    days = 80
+
+    for i in range(days):
+        print("test")
     
 
 

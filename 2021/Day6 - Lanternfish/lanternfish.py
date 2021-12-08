190f75408ba485c058a5aca61c40a4b0c43225e2
lemondrop81 <hacker814@hotmail.com>
2021-12-08 08:23:15 -0600

    Change input method once again

--- 2021/Day6 - Lanternfish/lanternfish.py
+++ 2021/Day6 - Lanternfish/lanternfish.py
@@ -1,10 +1,7 @@
 # Getting the text input
-def get_input():
-    with open("2021\Day6 - lanternfish\Day6.txt", 'r') as f:
-        data = [i for i in f]
-    return [int(i) for i in data[0].split(',')]
+fishInput = list(map(int,open("2021\Day6 - lanternfish\Day6.txt").read().split(',')))
 
 days = 80
    
 
-get_input()
+print(f)

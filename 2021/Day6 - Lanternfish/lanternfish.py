e02e8a0aff6d1dfba2d6c4ef87203aed520e017d
lemondrop81 <hacker814@hotmail.com>
2021-12-08 08:44:40 -0600

    Counts how many fish are at each age

--- 2021/Day6 - Lanternfish/lanternfish.py
+++ 2021/Day6 - Lanternfish/lanternfish.py
@@ -1,7 +1,24 @@
 # Getting the text input
 fishInput = list(map(int,open("2021\Day6 - lanternfish\Day6.txt").read().split(',')))
 
+# How many days
 days = 80
+
+# Creates the list to hold the end value
+fishlist = []
+
+# iterate through all the fish ages, to count
+for i in range(1, 8):
+
+    #Checks how many fish are of this age
+    if i in fishInput:
+        fishCounter = fishInput.count(i)
+        fishlist.append(fishCounter)
+
+    else:
+        fishlist.append(0)
+
+print(fishlist)
 
 for day in range(days):
     print("test")

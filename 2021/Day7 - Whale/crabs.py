5f2d7282a080c56b343e5848ed43b6ee62e87791
lemondrop81 <hacker814@hotmail.com>
2021-12-10 08:43:47 -0600

    Iterate through the loops

--- 2021/Day7 - Whale/crabs.py
+++ 2021/Day7 - Whale/crabs.py
@@ -2,7 +2,20 @@ import statistics
 data = list(map(int,open("2021\Day7 - Whale\Day7.txt").read().split(',')))
 
 def start():
-    print("test")
 
-    
+    # Calculate what the medium distance is
+    medium = round(statistics.median(data))
+    fuel = 0
+
+    # Loop through the range
+    for aim in range(medium - 5, medium + 5):
+        
+        #Loop through the crabs
+       for crab in data:
+           fuel += abs(crab - medium)
+
+    # Print out the final value
+    print(fuel)
+
+
 start()

c820070e09ab56063b0964f80e24931c96e5ee04
lemondrop81 <hacker814@hotmail.com>
2021-12-10 08:07:20 -0600

    Got the code to work, by switching to dictionary

--- 2021/Day6 - Lanternfish/lanternfish.py
+++ 2021/Day6 - Lanternfish/lanternfish.py
@@ -1,31 +1,38 @@
 # Getting the text input
-fishInput = list(map(int,open("2021\Day6 - lanternfish\Day6.txt").read().split(',')))
+data = list(map(int,open("2021\Day6 - lanternfish\Day6.txt").read().split(',')))
 
-# How many days
+# Initialize the dictionary
+data_dict = {}
+
+# Populate the dictionary
+for i in range(9):
+    data_dict[i] = 0
+    
+data_dict["6_new"] = 0
+
+# Loop through the number of fish    
+for fish in data:
+    data_dict[fish] += 1
+
+def update_dict(data_dict):
+    new_dict = {}
+
+    new_dict[6] = data_dict[0]
+    new_dict['6_new'] = data_dict[7]
+    new_dict[5] = data_dict[6] + data_dict['6_new']
+
+    for i in [1, 2, 3, 4, 5, 8]:
+        new_dict[i-1] = data_dict[i]
+
+    new_dict[8] = new_dict[6]
+    
+    return new_dict
+
+# initialize the number of days
 days = 80
 
-# Creates the list to hold the end value
-fishlist = []
+for day in range(0, days):
+    data_dict = update_dict(data_dict)
 
-# Sets the age new fish are created at
-fishBornAge = 8
+print(f"After {day+1} days: {sum(data_dict.values())} laternfishes.")
 
-# Sets the age for to reproduce
-fishRespawnAge = 6
-
-
-# iterate through all the fish ages, to count
-for i in range(1, 8):
-
-    #Checks how many fish are of this age
-    if i in fishInput:
-        fishCounter = fishInput.count(i)
-        fishlist.append(fishCounter)
-
-    else:
-        fishlist.append(0)
-
-print(fishlist)
-
-for day in range(days):
-    print("test")

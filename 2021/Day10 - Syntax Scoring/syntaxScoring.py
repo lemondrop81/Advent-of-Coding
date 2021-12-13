ec650feb2aceec6338ee8eb53c39a69acc5d8f69
lemondrop81 <hacker814@hotmail.com>
2021-12-13 08:15:28 -0600

    Setup the pairs and the number of points

--- 2021/Day10 - Syntax Scoring/syntaxScoring.py
+++ 2021/Day10 - Syntax Scoring/syntaxScoring.py
@@ -4,7 +4,14 @@ def start():
     text_file = open("2021\Day10 - Syntax Scoring\Day10.txt","r")
     lines = text_file.readlines()
     
+    # Determine the pairs
+    pairs = {"(": ")", "[": "]", "{": "}", "<": ">"}
+
+    # Determine the error points
+    points = {")": 3, "]": 57, "}": 1197, ">": 25137}
+    
     # Loop through all the input lines
     for i in lines:
+        score = 0
         print(i)
 start()

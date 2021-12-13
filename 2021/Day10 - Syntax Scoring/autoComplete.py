c0dab5986acb12e904aab7e2679d80bed2b159d0
lemondrop81 <hacker814@hotmail.com>
2021-12-13 08:59:32 -0600

    Add the scores to the score list

--- 2021/Day10 - Syntax Scoring/autoComplete.py
+++ 2021/Day10 - Syntax Scoring/autoComplete.py
@@ -1,35 +1,31 @@
 
-text_file = open("2021\Day10 - Syntax Scoring\Day10.txt","r")
-lines = text_file.readlines()
+lines = [x.strip() for x in open("2021\Day10 - Syntax Scoring\Day10.txt", 'r').readlines()]
 
 # opening and closing brackets
 OPENING = ['(', '[', '{', '<']
 CLOSING = [')', ']', '}', '>']
 
 # Determines the total of the scores
-score = []
+score_list = []
 
 # Loop through all the input lines
 for i in lines:
+    score = 0
     count = []
     error = False
-
     # Loop through each individual line to check if the brackets are the same
-    for c in i:
-        if c in OPENING:
-            count.append(c)
-
-        elif c in CLOSING and c == CLOSING[OPENING.index(count[-1])]:
-            count.pop
+    for y in i:
+        if y in OPENING:
+            count.append(y)
+        elif y in CLOSING and y == CLOSING[OPENING.index(count[-1])]:
+            count.pop()
         else:
             error = True
             break
-
+    # Manually set the value for the bracket
     if len(count) and not error:
         for z in count[::-1]:
-            # Times the score by 5
             score *= 5
-
             if z == '(':
                 score += 1
             elif z == '[':
@@ -38,6 +34,7 @@ for i in lines:
                 score += 3
             elif z == '<':
                 score += 4
-        
+        score_list.append(score)
 
-# Print the sum of the scores
+# Print the scores
+print(score_list)

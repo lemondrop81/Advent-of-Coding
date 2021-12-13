d3c7b1c8ae0672eeac7fde436fa3a5789126f83f
lemondrop81 <hacker814@hotmail.com>
2021-12-13 08:06:45 -0600

    iterate through the input lines

--- 2021/Day10 - Syntax Scoring/syntaxScoring.py
+++ 2021/Day10 - Syntax Scoring/syntaxScoring.py
@@ -3,5 +3,8 @@ def start():
     # Getting the text input
     text_file = open("2021\Day10 - Syntax Scoring\Day10.txt","r")
     lines = text_file.readlines()
-    print(lines)
+    
+    # Loop through all the input lines
+    for i in lines:
+        print(i)
 start()

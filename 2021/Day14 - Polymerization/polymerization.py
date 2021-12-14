3c6b5ef7b0e43474d2be523d3343baf1762ffec0
lemondrop81 <hacker814@hotmail.com>
2021-12-14 08:54:44 -0600

    Loop though the rules

--- 2021/Day14 - Polymerization/polymerization.py
+++ 2021/Day14 - Polymerization/polymerization.py
@@ -3,14 +3,18 @@ data = open("2021\Day14 - Polymerization\Day14.txt").read().split('\n')
 def split(word):
     return [char for char in word]
 
-for x in data:
-    y = x.split('->')
 t = split(data[0]) 
 
-# Initial value for the template loop
-i = 0
+for x in data:
+    y = x.split(' ')
 
-# iterate through the polymer template
-while i < len(t) - 1:
-    print(t[i] + t[i+1])
-    i += 1
+
+# Loop through the steps
+for loopCount in range(10):
+
+    # Initial value for the template loop
+    i = 0
+    # iterate through the polymer template
+    while i < len(t) - 1:
+        #print(t[i] + t[i+1])
+        i += 1

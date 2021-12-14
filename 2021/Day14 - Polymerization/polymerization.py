71121af317a3e43571fb8fbd34ff1bedd3ed7951
lemondrop81 <hacker814@hotmail.com>
2021-12-14 08:43:17 -0600

    Iterate through polymer

--- 2021/Day14 - Polymerization/polymerization.py
+++ 2021/Day14 - Polymerization/polymerization.py
@@ -6,3 +6,11 @@ def split(word):
 for x in data:
     y = x.split('->')
 t = split(data[0]) 
+
+# Initial value for the template loop
+i = 0
+
+# iterate through the polymer template
+while i < len(t) - 1:
+    print(t[i] + t[i+1])
+    i += 1

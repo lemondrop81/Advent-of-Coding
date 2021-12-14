8714b8d03532588cf4fe07916b0b1bd4ab50c163
lemondrop81 <hacker814@hotmail.com>
2021-12-14 08:34:37 -0600

    Split the input template into values

--- 2021/Day14 - Polymerization/polymerization.py
+++ 2021/Day14 - Polymerization/polymerization.py
@@ -1,4 +1,8 @@
 data = open("2021\Day14 - Polymerization\Day14.txt").read().split('\n')
 
+def split(word):
+    return [char for char in word]
+
 for x in data:
-    print(x)
+    y = x.split('->')
+t = split(data[0]) 

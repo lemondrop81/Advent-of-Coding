6c94558b138e7745aa4fd23a7cd1820c5bea95a7
lemondrop81 <hacker814@hotmail.com>
2021-12-15 08:35:59 -0600

    Complete day 1, initialize day 2

--- 2021/Day14 - Polymerization/polymerization.py
+++ 2021/Day14 - Polymerization/polymerization.py
@@ -2,10 +2,6 @@ from collections import Counter
 
 A, data = open("2021\Day14 - Polymerization\Day14.txt", 'r').read().split('\n\n')
 
-def split(word):
-    return [char for char in word]
-
-t = split(data[0]) 
 rules = []
 
 # Loop through and split the rules up
@@ -13,21 +9,26 @@ for x in data.splitlines():
      i, j = x.split(' -> ')
      rules.append((i,j))
 
-print(rules)
-
 # Counts the number of times these occur
 count = []
 
+# Loops a certain number of times
 for step in range(10):
     insert = []
+
+    # Loop through the polymer
     for i in range(len(A)):
+
+        # Loops through the rules, compared to the polymer
         for x in rules:
             if x[0] == A[i:i+2]:
                 insert.append(x[1])
                 break
-    for i, x in enumerate(range(1, len(t)+len(insert), 2)):
+    # Loops and inserts into the polymer
+    for i, x in enumerate(range(1, len(A)+len(insert), 2)):
         A = A[:x] + insert[i] + A[x:]
 
 
-mostCommon = Counter(count).most_common
-print(mostCommon)
+count = Counter(A)
+# print the output
+print(count.most_common()[0][1] - count.most_common()[-1][1])

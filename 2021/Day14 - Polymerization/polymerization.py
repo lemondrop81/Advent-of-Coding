1d682c29e1309f17adf032802abb17efd0940aca
lemondrop81 <hacker814@hotmail.com>
2021-12-15 08:00:24 -0600

    Loop through and count the polymers

--- 2021/Day14 - Polymerization/polymerization.py
+++ 2021/Day14 - Polymerization/polymerization.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 data = open("2021\Day14 - Polymerization\Day14.txt").read().split('\n')
 
 def split(word):
@@ -15,32 +17,36 @@ rules.pop(0)
 rules.pop(0)
 rules.pop()
 
+# Counts the number of times these occur
+count = []
+
 # Loop through the steps
-#for loopCount in range(10):
+for loopCount in range(10):
    
-# Initial value for the template loop
-i = 0
+    # Initial value for the template loop
+    i = 0
 
-# iterate through the polymer template
-while i < len(t):
+    # iterate through the polymer template
+    while i < len(t):
 
-    # iterate through the pair insertion rules
-    for rule in rules:
+        # iterate through the pair insertion rules
+        for rule in rules:
 
-        # Checks if the loop is going out of range
-        if i >= len(t) - 1:
-            break
+            # Checks if the loop is going out of range
+            if i >= len(t) - 1:
+                break
 
-        polymer = (t[i] + t[i+1])
-        if rule[0] == polymer:
-            # add the character to the middle of the list
-            print(polymer)
-            print(rule)
-            b = t[:]
-            b.insert(i+1, rule[2])
-            t = b
-            print(t)
-            i += 2
-        
+            polymer = (t[i] + t[i+1])
+            if rule[0] == polymer:
+                # add the character to the middle of the list
+                b = t[:]
+                b.insert(i+1, rule[2])
+                count += rule[2]
+                t = b
+                i += 2
+            
 
-    i += 1
+        i += 1
+
+mostCommon = Counter(count).most_common
+print(mostCommon)

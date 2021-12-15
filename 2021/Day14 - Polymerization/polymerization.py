ca2516ac51b1805d222fd75e6ac70f17c48ff8a7
lemondrop81 <hacker814@hotmail.com>
2021-12-15 08:15:57 -0600

    Changed how the polymer rules were formatted

--- 2021/Day14 - Polymerization/polymerization.py
+++ 2021/Day14 - Polymerization/polymerization.py
@@ -1,6 +1,6 @@
 from collections import Counter
 
-data = open("2021\Day14 - Polymerization\Day14.txt").read().split('\n')
+A, data = open("2021\Day14 - Polymerization\Day14.txt", 'r').read().split('\n\n')
 
 def split(word):
     return [char for char in word]
@@ -9,44 +9,25 @@ t = split(data[0])
 rules = []
 
 # Loop through and split the rules up
-for x in data:
-     rules.append(x.split(' -> '))
+for x in data.splitlines():
+     i, j = x.split(' -> ')
+     rules.append((i,j))
 
 print(rules)
-# remove the first and last strings
-rules.pop(0)
-rules.pop(0)
-rules.pop()
 
 # Counts the number of times these occur
 count = []
 
-# Loop through the steps
-for loopCount in range(10):
-   
-    # Initial value for the template loop
-    i = 0
-    # iterate through the polymer template
-    while i < len(t):
-
-        # iterate through the pair insertion rules
-        for rule in rules:
-
-            # Checks if the loop is going out of range
-            if i >= len(t) - 1:
+for step in range(10):
+    insert = []
+    for i in range(len(A)):
+        for x in rules:
+            if x[0] == A[i:i+2]:
+                insert.append(x[1])
                 break
+    for i, x in enumerate(range(1, len(t)+len(insert), 2)):
+        A = A[:x] + insert[i] + A[x:]
 
-            polymer = (t[i] + t[i+1])
-            if rule[0] == polymer:
-                # add the character to the middle of the list
-                b = t[:]
-                b.insert(i+1, rule[2])
-                count += rule[2]
-                t = b
-                i += 1
-            
-
-        i += 1
 
 mostCommon = Counter(count).most_common
 print(mostCommon)
